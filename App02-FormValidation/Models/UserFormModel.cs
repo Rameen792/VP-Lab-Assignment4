394af79ca9a1e437d2c9376deea54a3d9ee58628
Rameen792 <247249@students.au.edu.pk>
2026-05-31 17:12:02 +0500

    Add form model with validation attributes

---
+++ App02-FormValidation/Models/UserFormModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace App02_FormValidation.Models
+{
+    public class UserFormModel
+    {
+        [Required(ErrorMessage = "First Name is required.")]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Last Name is required.")]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
+        public string Email { get; set; } = string.Empty;
+    }
+}
