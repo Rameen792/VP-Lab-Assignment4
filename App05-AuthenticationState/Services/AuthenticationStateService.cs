2d7a54f9e61dc0f2817dae3f8c8d3463d98295ba
Rameen792 <247249@students.au.edu.pk>
2026-05-31 17:49:00 +0500

    implement the logic in service

--- App05-AuthenticationState/Services/AuthenticationStateService.cs
+++ App05-AuthenticationState/Services/AuthenticationStateService.cs
@@ -2,5 +2,25 @@
 {
     public class AuthenticationStateService
     {
+        public bool IsAuthenticated { get; private set; }
+
+        public event Action? OnChange;
+
+        public void LogIn()
+        {
+            IsAuthenticated = true;
+            NotifyStateChanged();
+        }
+
+        public void LogOut()
+        {
+            IsAuthenticated = false;
+            NotifyStateChanged();
+        }
+
+        private void NotifyStateChanged()
+        {
+            OnChange?.Invoke();
+        }
     }
 }
