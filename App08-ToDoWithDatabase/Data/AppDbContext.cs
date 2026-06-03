0230411fbc7644786117a00c836083a859d37444
Rameen792 <247249@students.au.edu.pk>
2026-06-03 21:15:04 +0500

    add data in data file

--- App08-ToDoWithDatabase/Data/AppDbContext.cs
+++ App08-ToDoWithDatabase/Data/AppDbContext.cs
@@ -1,6 +1,24 @@
-﻿namespace App08_ToDoWithDatabase.Data
+﻿using App08_ToDoWithDatabase.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace App08_ToDoWithDatabase.Data;
+
+public class AppDbContext : DbContext
 {
-    public class AppDbContext
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<TodoTask> TodoTasks => Set<TodoTask>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<TodoTask>(entity =>
+        {
+            entity.ToTable("todo_tasks");
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Title).IsRequired().HasMaxLength(500);
+            entity.Property(e => e.Priority).HasDefaultValue("Medium");
+            entity.Property(e => e.CreatedAt).HasDefaultValueSql("NOW()");
+            entity.Property(e => e.UpdatedAt).HasDefaultValueSql("NOW()");
+        });
     }
 }
