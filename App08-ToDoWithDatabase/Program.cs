82558e717805410a2989fe27995fb6e1e340363e
Rameen792 <247249@students.au.edu.pk>
2026-06-04 17:40:20 +0500

    program.cs of app8

--- App08-ToDoWithDatabase/Program.cs
+++ App08-ToDoWithDatabase/Program.cs
@@ -1,24 +1,18 @@
-using App08_ToDoWithDatabase.Components;
-using App08_ToDoWithDatabase.Data;
-using App08_ToDoWithDatabase.Services;
+using App08_ToDoList.Components;
+using App08_ToDoList.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add Razor + Blazor
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-// Add PostgreSQL via Npgsql EF Core
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Register TodoService as scoped
-builder.Services.AddScoped<TodoService>();
-
 var app = builder.Build();
 
-// Auto-create tables if they don't exist (optional, comment out if using SQL script)
+// Auto-create tables on startup
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
@@ -31,7 +25,7 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseStatusCodePagesWithReExecute("/not-found");
 app.UseHttpsRedirection();
 app.UseAntiforgery();
 app.MapStaticAssets();
