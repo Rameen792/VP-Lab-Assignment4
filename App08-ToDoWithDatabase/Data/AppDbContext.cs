b34d4d1ebd923979915fb5c8652e1f84fb244adc
Rameen792 <247249@students.au.edu.pk>
2026-06-05 10:38:53 +0500

    Remove corrupted App08 project

--- App08-ToDoWithDatabase/Data/AppDbContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using App08_ToDoList.Models;
-using Microsoft.EntityFrameworkCore;
-
-namespace App08_ToDoList.Data;
-
-public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
-{
-    public DbSet<TaskItem> Tasks => Set<TaskItem>();
-}
--- App08-ToDoWithDatabase/Models/TaskItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace App08_ToDoList.Models;
-
-public class TaskItem
-{
-    public int Id { get; set; }
-    public string Title { get; set; } = string.Empty;
-    public bool IsCompleted { get; set; } = false;
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-}
